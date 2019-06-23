592fd9553b0ccb5c7d88a1406aab6f6ce25d95f4
GeraMantecon <gerardo.mantecon@gmail.com>
2019-06-23 19:54:48 -0400

    Tutorial part II: Jinja and Inheritance

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 #The decorators associate the specified URLs to the view function.
@@ -5,4 +6,29 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    #Define a user variable.
+    user = {'username': 'Gerardo'}
+    #Define a list of posts.
+    posts = [
+    {
+        'author':'Pepe',
+        'body':'Esto es un mensaje de prueba!'
+    },
+    {   'author':'Guarumo',
+        'body':'Woof woof! Soy un perro?'
+    },
+    {
+        'author':'Genaro',
+        'body':'Buscando trabajo de dev!'
+    }
+    ]
+    #Function render_Template from flask renders an HTML file and assign the arguments.
+    return render_template('index.html', user=user, posts=posts)
+
+@app.route('/contact')
+def contact():
+    info = {'admin':'Gerardo Mantecon',
+            'mail':'gerardo@mail.com',
+            'phone':'1234567',
+            'address':'666 Fake St, Ontario, Canada.'}
+    return render_template('contact.html', info=info)
